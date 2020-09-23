feae60ca486a06f0fe51fd28efc9d5344a16369c
Epinder <epinder.singh@gmail.com>
2020-09-23 17:01:29 +0800

    product page

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./utils/path');
 
 const app = express();
 
@@ -7,12 +11,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found!</h1>');
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
