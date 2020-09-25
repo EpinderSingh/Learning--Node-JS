d64ee1153ac62174c379ff6ae20f38effa94cb68
Epinder <epinder.singh@gmail.com>
2020-09-25 10:14:11 +0800

    .

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const rootDir = require('./utils/path');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs',
+//   }),
+// );
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -20,7 +27,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
