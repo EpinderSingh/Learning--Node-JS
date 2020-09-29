4facc74a813b1c3fac1baaa7f14eaa986dbb7baf
Epinder <epinder.singh@gmail.com>
2020-09-29 17:56:00 +0800

    .

--- models/product.js
+++ models/product.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const products = [];
+const p = path.join(path.dirname(require.main.filename), 'data', 'product.json');
+
+const getProductsFromFile = (cb) => {
+
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
 
 module.exports = class Product {
     constructor(t) {
@@ -9,27 +22,16 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'product.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
-            })
+            });
         });
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'product.json');
-
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        })
+        getProductsFromFile(cb);
     }
 }
+
